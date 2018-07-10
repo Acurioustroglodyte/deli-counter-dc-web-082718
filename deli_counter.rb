d5fc01b83d01d5dc821c5cdfdd41eee5e90621b8
Trevor Jameson <tjameson15@gmail.com>
2018-07-10 10:50:14 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,15 @@
 # Write your code here.
+
+def line(arr)
+  if arr.size == 0
+    puts "The line is currently empty."
+  else
+    puts "The line is currently:"
+    arr.each do|i|
+      puts " #{arr.index(i)}. #{i}"
+    end
+  end
+end
+
+def take_a_number(line, name)
+end
