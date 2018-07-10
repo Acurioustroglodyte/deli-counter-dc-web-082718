a4760d20422f69e047f452e953f4221b97059360
Trevor Jameson <tjameson15@gmail.com>
2018-07-10 11:00:45 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,11 +1,11 @@
 def line(arr)
   output = "The line is currently:"
   if arr.size == 0
-    return "The line is currently empty."
+    puts "The line is currently empty."
   elsif arr.size > 0
     arr.each do |elem|
       output += "#{arr.index(elem)}. #{elem}"
     end
-    return output
+    puts output
   end
 end
